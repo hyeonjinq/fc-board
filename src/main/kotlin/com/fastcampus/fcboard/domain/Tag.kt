192d0fdd6fa1635e166233d920f20daef0873597
222472 <rion@plgrim.com>
2026-03-13 14:01:49 +0900

    feat : Optimize N+1 problem

--- src/main/kotlin/com/fastcampus/fcboard/domain/Tag.kt
+++ src/main/kotlin/com/fastcampus/fcboard/domain/Tag.kt
@@ -1,9 +1,12 @@
 package com.fastcampus.fcboard.domain
 
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
 @Entity
@@ -20,6 +23,7 @@ class Tag(
         protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
     var post: Post = post
         protected set
 }
